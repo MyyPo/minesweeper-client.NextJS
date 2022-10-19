a17cdcd3e02d247fc4cc9ed4945c1c64aefbc5d1
Myypo <nikirsmcgl@gmail.com>
2022-10-19 13:43:05 +0300

    Updated dependepcies and fixed imports

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,9 +1,7 @@
 import styles from "./Hero.module.css";
 import { useState, useEffect, useReducer } from "react";
-import { useSession } from "next-auth/react";
 
 import Game from "../Game";
-import Tile from "./tile";
 import Auth from "../Auth";
 import useWindowSize from "../../hooks/useWindowSize";
 import useAuthContext from "../../hooks/useAuthContext";
